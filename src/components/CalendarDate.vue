05265921059840d450961bd2bd48130e3c704b6b
quangdanhd <quangdanhd@gmail.com>
2024-04-11 22:16:51 +0700

    done add notes

--- src/components/CalendarDate.vue
+++ src/components/CalendarDate.vue
@@ -1,39 +1,42 @@
 <template>
+  <div id="calendar-month">{{ monthYear }}</div>
   <div id="calendar-dates">
     <div
       @click="showModal(item)"
       v-for="(item, key) in data"
       :key="key"
-      class="date-wrapper"
-      :class="{ 'active-day': item.today, 'active-month': item.activeMonth }"
+      class="date-wrapper active-month"
+      :class="{ 'active-day': item.isToday }"
     >
       <div class="date">
         <div class="date-day">
           <h2 class="date-day-of-month">{{ item.day }}</h2>
           <h2 class="date-day-of-week">{{ item.dayName }}</h2>
         </div>
-        <div v-if="item.activeMonth" class="date-title">
+        <div v-if="item.note" class="date-title">
           <div class="date-title-dot"></div>
-          <h2>Groundhog Day</h2>
+          <h2>{{ item.note }}</h2>
         </div>
       </div>
-      <div v-if="item.today" class="active-day-indicator"></div>
+      <div v-if="item.isToday" class="active-day-indicator"></div>
     </div>
   </div>
   <div v-if="modal" id="calendar-modal" @click.self="closeModal">
     <div class="modal">
       <div class="modal-header">
         <div class="date-day">
-          <h2 class="date-day-of-month">{{ modalDay }}</h2>
-          <h2 class="date-day-of-week">{{ modalDayName }}</h2>
+          <h2 class="date-day-of-month">
+            {{ modalData.day }} {{ modalData.monthName }}
+          </h2>
+          <h2 class="date-day-of-week">{{ modalData.dayName }}</h2>
         </div>
       </div>
       <div class="modal-body">
-        <textarea></textarea>
+        <textarea v-model="modalData.note"></textarea>
       </div>
       <div class="modal-footer">
         <button @click="closeModal">Close</button>
-        <button>Save</button>
+        <button @click="saveNote">Save</button>
       </div>
     </div>
   </div>
--- src/components/scripts/CalendarDate.js
+++ src/components/scripts/CalendarDate.js
@@ -2,42 +2,70 @@ export default {
   data() {
     return {
       data: [],
+      monthYear: null,
+      // modal
       modal: false,
-      modalDay: null,
-      modalDayName: null,
+      modalData: {},
+      // store
+      storeKey: "CALENDAR_STORE",
+      storedNotes: {},
     };
   },
   created() {
-    // today
-    const today = new Date();
-    // const today = new Date("2024-07-10");
-    const y = today.getFullYear();
-    const m = today.getMonth();
-    const currentDay = this.formatDate(today);
-    // first date of month
-    const first = new Date(y, m, 1);
-    // first date of calendar
-    const firstDate = new Date(
-      first.getTime() - (first.getDay() - 1) * 24 * 60 * 60 * 1000
-    );
-    // prepare for while
-    const currentMonth = `${y}-${m}`;
-    let date = firstDate;
-    let month = this.getYearMonth(date);
-    // data
-    this.data = [this.getDateData(date, currentDay, m)];
-    // eslint-disable-next-line no-constant-condition
-    while (true) {
-      date = new Date(date.getTime() + 24 * 60 * 60 * 1000);
-      month = this.getYearMonth(date);
-      // date is monday
-      if (month > currentMonth && date.getDay() === 1) {
-        break;
-      }
-      this.data.push(this.getDateData(date, currentDay, m));
-    }
+    this.getCalendar();
   },
   methods: {
+    getCalendar() {
+      this.storedNotes = this.getStore();
+      // today
+      const today = new Date();
+      this.getMonthYear(today);
+      // const today = new Date("2024-07-10");
+      const y = today.getFullYear();
+      const m = today.getMonth();
+      const currentDay = this.formatDate(today);
+      // first date of month
+      const first = new Date(y, m, 1);
+      // first date of calendar
+      const firstDate = new Date(
+        first.getTime() - (first.getDay() - 1) * 24 * 60 * 60 * 1000
+      );
+      // prepare for while
+      const currentMonth = `${y}-${m}`;
+      let date = firstDate;
+      let month = this.getYearMonth(date);
+      // data
+      this.data = [this.getDateData(date, currentDay)];
+      // eslint-disable-next-line no-constant-condition
+      while (true) {
+        date = new Date(date.getTime() + 24 * 60 * 60 * 1000);
+        month = this.getYearMonth(date);
+        // date is monday
+        if (month > currentMonth && date.getDay() === 1) {
+          break;
+        }
+        this.data.push(this.getDateData(date, currentDay));
+      }
+      console.table(this.data);
+    },
+    getStore() {
+      let store = localStorage.getItem(this.storeKey);
+      if (store) {
+        try {
+          store = JSON.parse(store);
+        } catch (e) {
+          console.log(e.message);
+          store = {};
+        }
+      } else {
+        store = {};
+      }
+      return store;
+    },
+    getMonthYear(date) {
+      this.monthYear =
+        date.toLocaleString("en", { month: "long" }) + " " + date.getFullYear();
+    },
     getYearMonth(date) {
       return date.getFullYear() + "-" + date.getMonth();
     },
@@ -48,25 +76,42 @@ export default {
 
       return [year, month, day].join("-");
     },
-    getDateData(date, today, month) {
+    getDateData(date, today) {
       const format = this.formatDate(date);
       const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+      let note = null;
+      if (this.storedNotes[format]) {
+        note = this.storedNotes[format];
+      }
 
       return {
         date: format,
         day: date.getDate(),
         dayName: days[date.getDay()],
-        today: format === today,
-        activeMonth: month === date.getMonth(),
+        isToday: format === today,
+        monthName: date.toLocaleString("en", { month: "short" }),
+        note: note,
       };
     },
     showModal(data) {
       this.modal = !this.modal;
-      this.modalDay = data.day;
-      this.modalDayName = data.dayName;
+      this.modalData = data;
     },
     closeModal() {
       this.modal = !this.modal;
+    },
+    saveNote() {
+      // get stored note
+      const store = this.getStore();
+      // write
+      store[this.modalData.date] = this.modalData.note;
+      // save
+      localStorage.setItem(this.storeKey, JSON.stringify(store));
+      // close modal
+      this.closeModal();
+      // refresh
+      this.getCalendar();
     },
   },
 };
